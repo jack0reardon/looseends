8b2348f2bf696a04b2defc6c52596969848f149f
Jack Reardon <jack0reardon@gmail.com>
2022-11-17 22:02:41 -0800

    Missed one

--- scripts/ancillary_functions.py
+++ scripts/ancillary_functions.py
@@ -1,18 +1,28 @@
 import os
 import re
 import scripts
+import networkx as nx
+import matplotlib.pyplot as plt
 
 
 def get_loose_ends(
     directory_or_file_path,
-    do_search_recursively = True
+    do_search_recursively = True,
+    do_show_graph = True
 ):
     file_paths = get_file_paths(directory_or_file_path, do_search_recursively)
 
-    for file_path in file_paths:
-        get_functions(file_path)
+    defined_funcs_and_dependencies = {}
 
-    return(file_paths)
+    for file_path in file_paths:
+        defined_funcs_and_dependencies.update(get_defined_funcs_and_dependencies(file_path))
+
+    only_defined_funcs_and_dependencies = get_only_defined_funcs_and_dependencies(defined_funcs_and_dependencies)
+
+    if do_show_graph:
+        get_loose_ends_graph(only_defined_funcs_and_dependencies)
+
+    return(only_defined_funcs_and_dependencies)
 
 
 
@@ -52,19 +62,48 @@ def get_standard_directory_path(directory_path):
         return(directory_path + '/')
 
 
-def get_functions(file_path):
-    print(file_path)
+def get_defined_funcs_and_dependencies(file_path):
     with open(file_path) as f:
         lines = f.read()
     
     func_defs = scripts.func_def_compiler.findall(lines)
 
-    # print(func_defs)
+    defined_funcs_and_dependencies = {}
 
     for func_def in func_defs:
-        print(func_def)
-
         # Parse called function
         called_functions = scripts.called_func_compiler.findall(func_def)
-        calling_function = called_functions[0]
-        print((calling_function, called_functions[1:]))
+        calling_function = called_functions[0] + '()'
+        defined_funcs_and_dependencies[calling_function] = [called_function + '()' for called_function in called_functions[1:]]
+
+    return(defined_funcs_and_dependencies)
+
+
+def get_only_defined_funcs_and_dependencies(defined_funcs_and_dependencies):
+    defined_funcs = list(defined_funcs_and_dependencies.keys())
+
+    # Only retain dependent functions that are one of the defined funcs
+    for defined_func in defined_funcs:
+        dependencies_to_retain = set(defined_funcs_and_dependencies[defined_func]).intersection(defined_funcs)
+        if len(dependencies_to_retain) == 0:
+            defined_funcs_and_dependencies.pop(defined_func, None)
+        else:
+            defined_funcs_and_dependencies[defined_func] = dependencies_to_retain
+
+    return(defined_funcs_and_dependencies)
+
+
+def get_loose_ends_graph(defined_funcs_and_dependencies):
+    directed_graph = nx.DiGraph()
+    for defined_func in defined_funcs_and_dependencies:
+        directed_graph.add_node(defined_func)
+        for dependency in defined_funcs_and_dependencies[defined_func]:
+            directed_graph.add_edge(dependency, defined_func)
+
+    pos = nx.shell_layout(directed_graph)
+    nx.draw_networkx(directed_graph, pos, **scripts.options_for_graph)
+    nx.draw(directed_graph, pos)
+    ax = plt.gca()
+    ax.margins(0.20)
+    plt.axis("off")
+    plt.show()
